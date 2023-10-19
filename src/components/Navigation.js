ba5307d56f720d2d6edc3ea3c0caf0f4ee3c07b9
TomateFarcie <leclerc.charlene@gmail.com>
2023-10-19 17:19:12 +0200

    test navbar responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import '../styles/nav.scss';
 
@@ -13,28 +13,55 @@ const Navigation = () => {
 
 
     return (
-        <header className='header'>
-            <Navbar collapseOnSelect expand="lg">
-                <Container>
-                    <div className="brand-box">
-                        <Link className='nav-brand' to='home' smooth={true} duration={100}>Portfolio</Link>
-                    </div>
-                    <Navbar.Collapse>
-                        <Nav className="sub-nav mr-auto d-block">
-                            <Nav.Link eventKey="1" as={Link} onClick={() => { handleClick('home') }}
-                                className={activeLink === 'home' ? 'nav-activ' : 'nav-link'}
-                                to='home' smooth={true} duration={100}>Accueil</Nav.Link>
-                            <Nav.Link eventKey="2" as={Link} onClick={() => { handleClick('about') }}
-                                className={activeLink === 'about' ? 'nav-activ' : 'nav-link'} to='about' smooth={true} duration={100}>A propos</Nav.Link>
-                            <Nav.Link eventKey="3" as={Link} onClick={() => { handleClick('projects') }}
-                                className={activeLink === 'projects' ? 'nav-activ' : 'nav-link'} to='projects' smooth={true} duration={100}>Projets</Nav.Link>
-                            <Nav.Link eventKey="4" as={Link} onClick={() => { handleClick('contact') }}
-                                className={activeLink === 'contact' ? 'nav-activ' : 'nav-link'} to='contact' smooth={true} duration={100}>Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+        <>
+            <Navbar expand="lg">
+                <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link eventKey={1} as={Link} href='#home'
+                            onClick={() => { handleClick('home') }}
+                            className={activeLink === 'home' ? 'nav-activ' : 'nav-link'}
+                            to='home'
+                            smooth={true}
+                            duration={100}>Accueil
+                        </ Nav.Link>
+                        <Nav.Link eventKey={2} as={Link} href='#about'
+                            onClick={() => { handleClick('about') }}
+                            className={activeLink === 'about' ? 'nav-activ' : 'nav-link'}
+                            to='about'
+                            smooth={true}
+                            duration={100}>A propos
+                        </Nav.Link>
+                        <Nav.Link eventKey={3} as={Link} href='#projects'
+                            onClick={() => { handleClick('projects') }}
+                            className={activeLink === 'projects' ? 'nav-activ' : 'nav-link'}
+                            to='projects'
+                            smooth={true}
+                            duration={100}>Projets
+                        </ Nav.Link>
+                        <Nav.Link eventKey={4} as={Link} href='#contact'
+                            onClick={() => { handleClick('contact') }}
+                            className={activeLink === 'contact' ? 'nav-activ' : 'nav-link'}
+                            to='contact'
+                            smooth={true}
+                            duration={100}>Contact
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-        </header>
+
+
+
+
+
+
+
+
+
+
+
+        </>
     );
 };
 
