c0944e23ed72ed88690a64779fd7a27a932b9213
TomateFarcie <leclerc.charlene@gmail.com>
2023-10-19 16:15:43 +0200

    test responsive navbar sur github

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -20,16 +20,16 @@ const Navigation = () => {
                         <Link className='nav-brand' to='home' smooth={true} duration={100}>Portfolio</Link>
                     </div>
                     <Navbar.Collapse>
-                        <Nav className="sub-nav">
-                            <Link onClick={() => { handleClick('home') }}
+                        <Nav className="sub-nav mr-auto d-block">
+                            <Nav.Link eventKey="1" as={Link} onClick={() => { handleClick('home') }}
                                 className={activeLink === 'home' ? 'nav-activ' : 'nav-link'}
-                                to='home' smooth={true} duration={100}>Accueil</Link>
-                            <Link onClick={() => { handleClick('about') }}
-                                className={activeLink === 'about' ? 'nav-activ' : 'nav-link'} to='about' smooth={true} duration={100}>A propos</Link>
-                            <Link onClick={() => { handleClick('projects') }}
-                                className={activeLink === 'projects' ? 'nav-activ' : 'nav-link'} to='projects' smooth={true} duration={100}>Projets</Link>
-                            <Link onClick={() => { handleClick('contact') }}
-                                className={activeLink === 'contact' ? 'nav-activ' : 'nav-link'} to='contact' smooth={true} duration={100}>Contact</Link>
+                                to='home' smooth={true} duration={100}>Accueil</Nav.Link>
+                            <Nav.Link eventKey="2" as={Link} onClick={() => { handleClick('about') }}
+                                className={activeLink === 'about' ? 'nav-activ' : 'nav-link'} to='about' smooth={true} duration={100}>A propos</Nav.Link>
+                            <Nav.Link eventKey="3" as={Link} onClick={() => { handleClick('projects') }}
+                                className={activeLink === 'projects' ? 'nav-activ' : 'nav-link'} to='projects' smooth={true} duration={100}>Projets</Nav.Link>
+                            <Nav.Link eventKey="4" as={Link} onClick={() => { handleClick('contact') }}
+                                className={activeLink === 'contact' ? 'nav-activ' : 'nav-link'} to='contact' smooth={true} duration={100}>Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
